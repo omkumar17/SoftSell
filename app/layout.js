c755bf7c7027c2892229b03c3689fa01e4eeedfc
OmKumar <omkumar1870@gmail.com>
2025-05-11 22:05:17 +0530

    favicon changed

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+        <link rel="icon" href="/favicon.png" />
       </head>
       <body id="body" className="font-sans antialiased">
         {children}
